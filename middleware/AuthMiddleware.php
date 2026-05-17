29f5a4674699899f03c594f0079d5abcc5e97589
Ratul12 <ratulmueid@gmail.com>
2026-05-17 23:34:05 +0600

    AUth Middleware file

---
+++ middleware/AuthMiddleware.php
@@ -0,0 +1,34 @@
+<?php
+class AuthMiddleware {
+    public static function checkModerator() {
+        if (session_status() == PHP_SESSION_NONE) {
+            session_start();
+        }
+        if (!isset($_SESSION['user_id'])) {
+            header("Location: ../../login.php");
+            exit();
+        }
+        if ($_SESSION['role'] !== 'moderator') {
+            header("Location: ../../login.php");
+            exit();
+        }
+    }
+
+    public static function getSession() {
+        if (session_status() == PHP_SESSION_NONE) {
+            session_start();
+        }
+        return $_SESSION;
+    }
+
+    public static function logout() {
+        if (session_status() == PHP_SESSION_NONE) {
+            session_start();
+        }
+        session_unset();
+        session_destroy();
+        header("Location: ../../login.php");
+        exit();
+    }
+}
+?>
